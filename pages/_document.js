ca3ef7d3d0fd48ac99cc7b1d7dd50056a08f9072
Testando0 <joh769637@gmail.com>
2025-12-02 05:59:38 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,10 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Aqui você pode adicionar meta tags, ícones ou links globais */}
-        <meta name="description" content="Restaurador de Fotos com IA em Next.js e Replicate" />
+        <meta name="description" content="Restaurador de Fotos IA" />
       </Head>
       <body>
-        {/* Onde todo o conteúdo de pages/index.js será injetado */}
-        <Main /> 
+        <Main />
         <NextScript />
       </body>
     </Html>
